c01b7eaac2eed2e28811ed4bfa4fe5238bdd7cf9
Amleth36 <andrew_snare@hotmail.com>
2021-07-02 14:52:42 +1000

    animations

--- HorrorGame/Assets/2D Scene/2D Scripts/PlayerControl.cs
+++ HorrorGame/Assets/2D Scene/2D Scripts/PlayerControl.cs
@@ -23,6 +23,22 @@ public class PlayerControl : MonoBehaviour
     bool itemlock = false;
     public bool itemUseBool = false;
 
+    private float movementSpeed;
+    float horizontal;
+    private Rigidbody2D myRigidbody;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        movementSpeed = 5;
+        horizontal = 0;
+        target = new Vector2(transform.position.x, transform.position.y);
+
+        anim = GetComponentInChildren<Animator>();
+    }
+
     IEnumerator Pickup()
     {
         //play pickup animation
@@ -60,98 +76,142 @@ public class PlayerControl : MonoBehaviour
         InspectText.enabled = false;
     }
 
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Getting the coordinates of the mouseposition in game
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        //Check if the user left clicks and get coords of the click
-        if (Input.GetMouseButtonDown(0))
+        if (collision.gameObject.tag == "light")
         {
-            //store the location
-            target = new Vector2(mousePos.x, transform.position.y);
-            //detect what you hit
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
-            if (hit)
-            {
-                if (hit.collider.gameObject.tag == "door")                          //All clickable items need a collider on them and their tag appropriately set
-                {
-                    doorObj = hit.collider.gameObject;  //store the doorway you just clicked
-                }
-                if (hit.collider.gameObject.tag == "item")
-                {
-                    itemObj = hit.collider.gameObject;  //store the item you just clicked
-                }
-                if (hit.collider.gameObject.tag == "Inventory")
-                {
-                    target = new Vector2(transform.position.x, transform.position.y);    //Nullify movement if you click on the inventory
-                }
-                if (hit.collider.gameObject.tag == "Interactable")
-                {
-                    UseObj = hit.collider.gameObject;   //store the selected world object to use an item on
-                }
-            }
+            Debug.Log("In Light");
         }
-        //move player toward target coords
-        if (!itemUseBool)
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "light")
         {
-            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
+            Debug.Log("Exiting Light");
+        }
+    }
 
-            if (doorObj != null)
+    void FixedUpdate()
+    {
+        //float horizontal = (Input.GetKey(KeyCode.A)) ? -1f : (Input.GetKey(KeyCode.D)) ? 1f : 0f;
+        if (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.D)))
+        {
+            if (Input.GetKey(KeyCode.A))
             {
-                if (Vector2.Distance(doorObj.transform.position, transform.position) < 1.2f)    //only do the action when close to door. Just walk to it until then
-                {
-                    //load scene through doorway
-                }
+                horizontal = -1f;
+                anim.SetBool("isMoving", true);
             }
-            if (itemObj != null)
+            if (Input.GetKey(KeyCode.D))
             {
-                if (itemlock == false)
-                {
-                    if (Vector2.Distance(itemObj.transform.position, transform.position) < 1.2f)    //only do the action when close to door. Just walk to it until then
-                    {
-                        StartCoroutine(Pickup());
-                        itemlock = true;
-                    }
-                }
+                horizontal = 1f;
+                anim.SetBool("isMoving", true);
             }
         }
         else
-        {               //using an item movement
-            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
-
-            if (UseObj != null)
-            {
-                if (itemlock == false)
-                {
-                    if (Vector2.Distance(UseObj.transform.position, transform.position) < 1.2f)    //only do the action when close to door. Just walk to it until then
-                    {
-                        switch (UseObj.name)   //switch statement to find the prefab of the item's namesake
-                        {
-                            case "Green Interactible":
-                                if (storedUseItemRef == "Circle")
-                                {
-                                    InspectText.text = "I can combine these two.";
-                                    StartCoroutine(UseTextEnum());
-                                    itemUseBool = false;
-                                    itemlock = true;
-                                    break;
-                                }
-                                else
-                                {
-                                    InspectText.text = "That doesn't work with that.";
-                                    StartCoroutine(UseTextEnum());
-                                    itemUseBool = false;
-                                    itemlock = true;
-                                    break;
-                                }
-                        }
-                    }
-                }
-            }
-            else
-                itemUseBool = false;
+        {
+            horizontal = 0f;
+            anim.SetBool("isMoving", false);
         }
+        HandleMovement(horizontal);
     }
+
+    private void HandleMovement(float horizontal)
+    {
+        myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+    }
+
+    //void Update()
+    //{
+    //    //Getting the coordinates of the mouseposition in game
+    //    Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    //    //Check if the user left clicks and get coords of the click
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        //store the location
+    //        target = new Vector2(mousePos.x, transform.position.y);
+    //        //detect what you hit
+    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+    //        if (hit)
+    //        {
+    //            if (hit.collider.gameObject.tag == "door")                          //All clickable items need a collider on them and their tag appropriately set
+    //            {
+    //                doorObj = hit.collider.gameObject;  //store the doorway you just clicked
+    //            }
+    //            if (hit.collider.gameObject.tag == "item")
+    //            {
+    //                itemObj = hit.collider.gameObject;  //store the item you just clicked
+    //            }
+    //            if (hit.collider.gameObject.tag == "Inventory")
+    //            {
+    //                target = new Vector2(transform.position.x, transform.position.y);    //Nullify movement if you click on the inventory
+    //            }
+    //            if (hit.collider.gameObject.tag == "Interactable")
+    //            {
+    //                UseObj = hit.collider.gameObject;   //store the selected world object to use an item on
+    //            }
+    //        }
+    //    }
+    //    //move player toward target coords
+    //    if (!itemUseBool)
+    //    {
+    //        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
+
+    //        if (doorObj != null)
+    //        {
+    //            if (Vector2.Distance(doorObj.transform.position, transform.position) < 1.2f)    //only do the action when close to door. Just walk to it until then
+    //            {
+    //                //load scene through doorway
+    //            }
+    //        }
+    //        if (itemObj != null)
+    //        {
+    //            if (itemlock == false)
+    //            {
+    //                if (Vector2.Distance(itemObj.transform.position, transform.position) < 1.2f)    //only do the action when close to item. Just walk to it until then
+    //                {
+    //                    StartCoroutine(Pickup());
+    //                    itemlock = true;
+    //                }
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {               //using an item movement
+    //        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
+
+    //        if (UseObj != null)
+    //        {
+    //            if (itemlock == false)
+    //            {
+    //                if (Vector2.Distance(UseObj.transform.position, transform.position) < 1.2f)    //only do the action when close to door. Just walk to it until then
+    //                {
+    //                    switch (UseObj.name)   //switch statement to find the prefab of the item's namesake
+    //                    {
+    //                        case "Green Interactible":
+    //                            if (storedUseItemRef == "Circle")
+    //                            {
+    //                                InspectText.text = "I can combine these two.";
+    //                                StartCoroutine(UseTextEnum());
+    //                                itemUseBool = false;
+    //                                itemlock = true;
+    //                                break;
+    //                            }
+    //                            else
+    //                            {
+    //                                InspectText.text = "That doesn't work with that.";
+    //                                StartCoroutine(UseTextEnum());
+    //                                itemUseBool = false;
+    //                                itemlock = true;
+    //                                break;
+    //                            }
+    //                    }
+    //                }
+    //            }
+    //        }
+    //        else
+    //            itemUseBool = false;
+    //    }
+    //}
 }
